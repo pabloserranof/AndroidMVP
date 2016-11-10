9abb2e82ea19cb0074c846731eb596e915e8d978
Developer <pabloserranof@gmail.com>
2016-11-10 14:27:51 +0100

    Add leakcanary

--- app/src/main/java/com/pabloserrano/androidmvp/MyApplication.java
+++ app/src/main/java/com/pabloserrano/androidmvp/MyApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.pabloserrano.androidmvp.module.AppModule;
 import com.pabloserrano.androidmvp.module.NetModule;
 import com.pabloserrano.androidmvp.module.RepositoryModule;
+import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
 
@@ -15,6 +16,12 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         component = DaggerAppComponent.builder()
                 .appModule(new AppModule(this))
